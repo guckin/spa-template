19eb6a46c6e8075227e5890b4db44e6e0fea9457
Stephen McGuckin <stephen.mcguckin@nordicsemi.no>
2022-12-08 14:10:13 -0800

    boilerplate

---
+++ infrastructure/create-stack.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { TestAppStack } from './app-stack';
+
+const app = new cdk.App();
+new TestAppStack(app, 'TestAppStack', {
+  env: {
+    region: 'us-west-2',
+    account: '084882962555'
+  },
+});
